9d1b86f7cd1e2f6378ac647b8caec448311d97bd
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-02-03 22:32:39 +0000

    adding hover effect

---
+++ ressources/js/main.js
@@ -0,0 +1,6 @@
+// let cardsShop = document.querySelector('.cards-shop')
+
+// cardsShop.addEventListener('mouseover',()=>{
+//     cardsShop.style.backgroundSize = '130%'
+//     cardsShop.style.transition = 'all 1s'
+// })
