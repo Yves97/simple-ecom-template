093e2ebb23d07625fca837a1e1f1fd73772c3067
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-02-04 12:33:30 +0000

    details page

--- ressources/js/main.js
+++
@@ -1,6 +0,0 @@
-// let cardsShop = document.querySelector('.cards-shop')
-
-// cardsShop.addEventListener('mouseover',()=>{
-//     cardsShop.style.backgroundSize = '130%'
-//     cardsShop.style.transition = 'all 1s'
-// })
